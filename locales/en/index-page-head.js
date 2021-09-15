42b4bf2c5b67a625c866b64ebc6547e037a5cb92
Michael Paulukonis <xraysmalevich@gmail.com>
2021-09-14 22:25:28 -0400

    Restore title and description

--- locales/en/index-page-head.js
+++ locales/en/index-page-head.js
@@ -1,4 +1,4 @@
 export default {
-  title: 'Marina Aísa - Product Designer and Front-End Developer',
-  description: 'I’m a Product Designer (UI/UX) and Front-End Developer based in Barcelona.'
+  title: 'Michael J. Paulukonis - Software Architect, Web Developer, and Code Artists',
+  description: 'I’m a Full-Stack Developer and Architect living in Metro-West Boston.'
 }
