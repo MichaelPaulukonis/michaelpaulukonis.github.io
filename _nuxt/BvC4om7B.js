b19576096f93cf5c3a8b2983767cffee678e61db
MichaelPaulukonis <xraysmalevich@gmail.com>
2025-12-22 09:25:42 -0500

    Updates

--- _nuxt/BvC4om7B.js
+++
@@ -1,1 +0,0 @@
-import{i as t,c as o,L as s,o as a}from"./DfQNfkma.js";const n={class:"index-page"},_=t({__name:"default",setup(r){return(e,c)=>(a(),o("div",n,[s(e.$slots,"default")]))}});export{_ as default};
--- _nuxt/DX4F_hZz.js
+++
@@ -1,1 +0,0 @@
-import{i as o,c as s,L as t,o as a}from"./DfQNfkma.js";const n={class:"index-page"},l=o({__name:"purple",setup(r){return(e,c)=>(a(),s("div",n,[t(e.$slots,"default")]))}});export{l as default};
