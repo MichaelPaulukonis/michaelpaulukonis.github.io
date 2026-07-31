a214d9f27ad9a381bdb872c8128306ada91d6ffa
MichaelPaulukonis <xraysmalevich@gmail.com>
2026-07-30 23:59:22 -0400

    feat: add BlogFeatured component for blog index hero post

---
+++ app/components/blog/featured.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="rounded-md border-2 border-typography_primary hover--border-brand_primary group relative mb-8">
+        <NuxtLink :to="article.path + '/'" class="p-6 block relative">
+            <header>
+                <h2 class="blog-featured__title text-h2 leading-h2 font-semibold mb-3">
+                    {{ article.headline }}
+                </h2>
+                <p class="blog-featured__date mb-4">
+                    {{ $formatDate(article.date) }}
+                </p>
+                <p class="text-typography_primary">{{ article.description }}</p>
+            </header>
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    article: {
+        type: Object,
+        required: true
+    }
+});
+const { $formatDate } = useNuxtApp();
+</script>
+
+<style scoped>
+.blog-featured__title {
+    display: inline-block;
+    padding: 3px 10px;
+    background-color: #7d5cff;
+    color: #fcfcfc;
+    border-radius: 2px;
+}
+
+.blog-featured__date {
+    display: inline-block;
+    padding: 2px 8px;
+    background: #38ef7d;
+    color: #000;
+    font-size: 0.875rem;
+}
+</style>
